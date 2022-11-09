071fe73a3a1be1fed327d3710932ef7d1126feb4
Divyank <19bt04047@gsfcuniversity.ac.in>
2022-11-09 16:28:38 +0530

    Camera Commit

--- app/src/main/java/com/example/smartsolutionforfarmers/Detection.java
+++ app/src/main/java/com/example/smartsolutionforfarmers/Detection.java
@@ -1,14 +1,58 @@
 package com.example.smartsolutionforfarmers;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Detection extends AppCompatActivity {
+
+    ImageView imageView;
+    Button btOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detection);
+
+        imageView = findViewById(R.id.imageView);
+        btOpen = findViewById(R.id.button);
+
+        //Request for camera permission
+        if (ContextCompat.checkSelfPermission(Detection.this,
+                Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(Detection.this,
+                    new String[]{
+                            Manifest.permission.CAMERA
+                    }, 100);
+        }
+
+        btOpen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 100);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) {
+            Bitmap captureImage = (Bitmap) data.getExtras().get("data");
+
+            imageView.setImageBitmap(captureImage);
+        }
     }
 }
